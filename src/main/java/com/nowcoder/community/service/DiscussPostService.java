9867e301615407b2a549cdafc18ccb3a8505f84e
Katniss1025 <819147806@qq.com>
2023-08-31 22:39:52 +0800

    增加查看帖子详情功能

--- src/main/java/com/nowcoder/community/service/DiscussPostService.java
+++ src/main/java/com/nowcoder/community/service/DiscussPostService.java
@@ -39,6 +39,10 @@ public class DiscussPostService {
         post.setContent(sensitiveFilter.filter(post.getContent()));
 
         return discussPostMapper.insertDiscussPost(post);
-    };
+    }
+
+    public DiscussPost findDiscussPostById(int id){
+        return discussPostMapper.selectDiscussPostById(id);
+    }
 
 }
