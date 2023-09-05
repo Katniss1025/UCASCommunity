7caabc696e4c09aa22977b0c1343b00a82dad05d
Katniss1025 <819147806@qq.com>
2023-09-05 10:10:40 +0800

    新增发送私信功能，已读功能

---
+++ src/main/java/com/nowcoder/community/controller/CommentController.java
@@ -0,0 +1,41 @@
+package com.nowcoder.community.controller;
+
+import com.nowcoder.community.entity.Comment;
+import com.nowcoder.community.entity.User;
+import com.nowcoder.community.service.CommentService;
+import com.nowcoder.community.service.DiscussPostService;
+import com.nowcoder.community.util.HostHolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Date;
+
+@Controller
+@RequestMapping("/comment")
+
+public class CommentController {
+
+    @Autowired
+    private CommentService commentService;
+
+    @Autowired
+    private HostHolder hostHolder;
+
+    @RequestMapping(path = "/add/{discussPostId}", method = RequestMethod.POST)
+    public String addComment(@PathVariable("discussPostId") int discussPostId, Comment comment){
+        // 获取用户
+        User user = hostHolder.getUser();
+//        if(user == null) return 0;
+        comment.setUserId(user.getId());
+        comment.setStatus(0);
+        comment.setCreateTime(new Date());
+        System.out.println(user.getUsername());
+        commentService.addComment(comment);
+
+        return "redirect:/discuss/detail/" + discussPostId;
+    }
+
+}
--- src/main/java/com/nowcoder/community/service/CommentService.java
+++ src/main/java/com/nowcoder/community/service/CommentService.java
@@ -1,18 +1,32 @@
 package com.nowcoder.community.service;
 
 import com.nowcoder.community.dao.CommentMapper;
+import com.nowcoder.community.dao.DiscussPostMapper;
 import com.nowcoder.community.entity.Comment;
+import com.nowcoder.community.util.CommunityConstant;
+import com.nowcoder.community.util.SensitiveFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.util.HtmlUtils;
 
 import java.util.List;
 
 @Service
 
-public class CommentService {
+public class CommentService implements CommunityConstant{
 
     @Autowired
     private CommentMapper commentMapper;
+
+    @Autowired
+    private SensitiveFilter sensitiveFilter;
+
+    @Autowired
+    private DiscussPostService discussPostService;
+
 
     public List<Comment> findCommentsByEntity(int entityType, int entityId, int offset, int limit){
         return commentMapper.selectCommentsByEntity(entityType,entityId,offset,limit);
@@ -22,4 +36,22 @@ public class CommentService {
         return commentMapper.selectCountByEntity(entityType,entityId);
     }
 
+    @Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED)
+    public int addComment(Comment comment){
+        if(comment == null){
+            throw new IllegalArgumentException("参数不能为空");
+        }
+        // 添加评论，过滤敏感词和html标签
+        comment.setContent(HtmlUtils.htmlEscape(comment.getContent()));
+        comment.setContent(sensitiveFilter.filter(comment.getContent()));
+        int rows = commentMapper.insertComment(comment);
+
+        // 更新帖子评论数量
+        if(comment.getEntityType()==ENTITY_TYPE_POST){ // 一级评论
+            int count = commentMapper.selectCountByEntity(comment.getEntityType(), comment.getEntityId());
+            discussPostService.updateCommentCount(comment.getTargetId(), count);
+        }
+
+        return rows;
+    }
 }
